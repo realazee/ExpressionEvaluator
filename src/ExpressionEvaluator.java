96eecff0f7002a11be8a75bf4635328711d4e647
Aaron Zheng <100024608@mvla.net>
2020-10-23 15:51:55 -0700

    Everything but negation working. ExpressionEvaluator mostly complete.

--- src/ExpressionEvaluator.java
+++ src/ExpressionEvaluator.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -22,13 +25,12 @@ import javafx.stage.Stage;
 public class ExpressionEvaluator extends Application {
 
 	/** The data stack. */
-	private GenericStack<Integer> dataStack = new GenericStack<Integer>();
+	private GenericStack<Double> dataStack = new GenericStack<Double>();
 
 	/** The operator stack. */
 	private GenericStack<String>  operStack = new GenericStack<String>();;
 
 	/** The output results that will be displayed to the user */
-	private Text outputResults;
 
 	/**
 	 * Start.
@@ -39,10 +41,9 @@ public class ExpressionEvaluator extends Application {
 	private GridPane main = new GridPane();
 
 	private TextField expressionField;
-	private String answer;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 
 		Scene scene = new Scene(main, 400, 400);
 		Label textLabel = new Label("Enter data here");
@@ -81,70 +82,214 @@ public class ExpressionEvaluator extends Application {
 	 *         expression and the value that it equals, or indicate if some error occurred.
 	 */
 	private String[] strArray;
-	private int d1;
-	private int d2;
+	private double d1;
+	private double d2;
+	
 	protected String evaluateExpression(String str) {
-		str = str.replaceAll("([+-/\\(\\)\\*])", " $1 ");
+		//implicit muliplication string processor
+		for(int i = 0; i < str.length() - 1; i++) {
+			if(isNumber(str.substring(i, i+1)) && str.substring(i+1, i+2).equals("(")) {
+				str = addChar(str, '*', i+1);
+			}
+			if(str.substring(i, i+1).equals(")") && str.substring(i+1, i+2).equals("(")) {
+				str = addChar(str, '*', i+1);
+			}
+			if(str.substring(i, i+1).equals(")") && isNumber(str.substring(i+1, i+2))) {
+				str = addChar(str, '*', i+1);
+			}
+		}
+		
+		
+		str = str.replaceAll("(\\+|-|/|\\(|\\)|\\*)", " $1 ");
 		str = str.trim();
 		String orgInput = str;
 		strArray = str.split("\\s+");
-
-
-		//this is still not fully operational.
-		for(int i = 0; i < strArray.length; i++) {
+		
+		
+		//negation processor
+		/*
+		int countDifference = 0;
+		String[] temp;
+		String tempString;
+		for(int i = 0; i < strArray.length-2; i++) {
+			if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("/") || strArray[i].equals("*")) {
+				if(strArray[i+1].equals("-")) {
+					countDifference++;
+				}
+			}
+		}
+		temp = new String[strArray.length-countDifference];
+		
+		for(int i = 0; i < strArray.length-2; i++) {
+			if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("/") || strArray[i].equals("*")) {
+				if(strArray[i+1].equals("-")) {
+					tempString = "-"+strArray[i+2];
+					temp[i] = (strArray[i]);
+					temp[i+1] = tempString;
+					i= i+2;
+				}
+				else {
+					temp[i] = strArray[i];
+				}
+			}
+			else{
+				temp[i] = strArray[i];
+			}
+			
+		}
+		strArray = temp;
+		*/
+		
+		/*
+		ArrayList<String> temp = new ArrayList<String>();
+		
+		String tempString;
+		for(int i = 0; i < strArray.length-2; i++) {
+			if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("/") || strArray[i].equals("*")) {
+				if(strArray[i+1].equals("-")) {
+					tempString = "-"+strArray[i+2];
+					temp.add(strArray[i]);
+					temp.add(tempString);
+					i= i+2;
+				}
+				else {
+					temp.add(strArray[i]);
+				}
+			}
+			else if(i == strArray.length - 2) {
+				temp.add(strArray[i]);
+				temp.add(strArray[i+1]);
+				temp.add(strArray[i+2]);
+				break;
+			}
+			else{
+				temp.add(strArray[i]);
+			}
+			
+			
+		}
+		String[] tempStrArray = new String[temp.size()];
+		for(int i = 0; i < temp.size(); i++) {
+			tempStrArray[i] = temp.get(i);
+		}
+		strArray = tempStrArray;
+		System.out.println(Arrays.toString(strArray));
+		
+*/
+		//op error checking
+		if(strArray[strArray.length - 1].equals("*")|| strArray[strArray.length-1].equals("/")||strArray[strArray.length-1].equals("-")||strArray[strArray.length-1].equals("+")) {
+			System.out.println("op errored 1");
+			return "Op Error: " + orgInput;
+		}
+		for(int i = 0; i < strArray.length-2; i++) {
+			if(strArray[i].equals("-") && strArray[i+1].equals("-") && strArray[i+2].equals("-")) {
+				return "Op Error: " + orgInput;
+			}
+		}
+		
+		for(int i = 0; i < strArray.length-1; i++) {
 			//System.out.println(strArray[i]);
+			if(i == 0 && !strArray[i].equals("(")) {
+				if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("*") || strArray[i].equals("/")) {
+					System.out.println("op errored 2");
+					return "Op Error: " + orgInput;
+				}
+			}
+			if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("*") || strArray[i].equals("/") || strArray[i].equals("(")) {
+				if(strArray[i+1].equals("+") ||strArray[i+1].equals("*") || strArray[i+1].equals("/") || strArray[i+1].equals(")")) {
+					System.out.println("op errored 3");
+					return "Op Error: " + orgInput;
+				}
+			}
+
+		}
+		
+
+		//paren error checking
+		int parenCount =0;
+		for(int i = 0; i < strArray.length; i++) {
+			if(strArray[i].equals("(")) {
+				parenCount++;
+			}
+			if(strArray[i].equals(")")) {
+				parenCount--;
+			}
+		}
+		System.out.println(parenCount);
+		if(parenCount != 0) {
+			return "Paren Error:" + orgInput;
 		}
 
+		//data error checking
+		System.out.println(Arrays.toString(strArray));
 		for(int i = 0; i < strArray.length; i++) {
-		//	System.out.println("dataStack = " + dataStack.toString());
+			if(strArray[i].contains("f") || strArray[i].contains("d")) {
+				return "Data Error: " + orgInput;
+			}
+			if(!strArray[i].equals("(") && !strArray[i].equals(")") && !strArray[i].equals("+") && !strArray[i].equals("-") && !strArray[i].equals("*") && !strArray[i].equals("/")) {
+				if(!isNumber(strArray[i])) {
+					return "Data Error: " + orgInput;
+				}
+			}		
+		}
+		for(int i = 0; i < strArray.length - 1; i++) {
+			if(isNumber(strArray[i]) && isNumber(strArray[i+1])){
+				return "Data Error: " + orgInput;
+			}
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//evaluation block
+		double evalResult;
+		for(int i = 0; i < strArray.length; i++) {
+			//	System.out.println("dataStack = " + dataStack.toString());
 			//System.out.println("operStack = " + operStack.toString());
 
-			if(strArray[i].equals("(")) {
-				//System.out.println("its a (");
+			if(strArray[i].equals("(")) {				
+				operStack.push(strArray[i]);
+			}
+			else if(strArray[i].equals(")")) {
+				while(!operStack.peek().equals("(")) {
+					evalResult = evaluateTopOfStack();
+					if(Double.isInfinite(evalResult)) {
+						return "Div0 Error:" + orgInput;
+					}
+					dataStack.push(evalResult);				
+				}
+				operStack.pop();				
+			}
+			else if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("*") ||strArray[i].equals("/")) {
+
+				while(!shouldPush(strArray[i]) && dataStack.getSize() >=2) {
+					evalResult = evaluateTopOfStack();
+					if(Double.isInfinite(evalResult)) {
+						return "Div0 Error:" + orgInput;
+					}
+					dataStack.push(evalResult);		
+				}
 				operStack.push(strArray[i]);
 			}
 
-			else if(strArray[i].equals(")")) {
-				while(!operStack.peek().equals("(")) {
-					dataStack.push(evaluateTopOfStack());
-
-					
-				}
-				operStack.pop();
-				
-			}
-
-			else if(strArray[i].equals("+") || strArray[i].equals("-") || strArray[i].equals("*") ||strArray[i].equals("/")) {
-				
-				//System.out.println("its in here");
-				/*
-				if(operStack.isEmpty() || operStack.peek().equals("(")) {
-					operStack.push(strArray[i]);
-					alreadyPushed = true;
-				}
-*/
-				while(!shouldPush(strArray[i]) && dataStack.getSize() >=2) {
-					//System.out.println("looped");
-
-					dataStack.push(evaluateTopOfStack());
-
-
-				}
-				
-					operStack.push(strArray[i]);
-				
-			}
-
 			else {
-				//System.out.println("it got here");
-				dataStack.push(Integer.parseInt(strArray[i]));
+
+				dataStack.push(Double.parseDouble(strArray[i]));
 
 			}
 		}
-
-
 		while(operStack.isEmpty() == false && dataStack.getSize() >= 2) {
-			dataStack.push(evaluateTopOfStack());
+			evalResult = evaluateTopOfStack();
+			if(Double.isInfinite(evalResult)) {
+				return "Div0 Error:" + orgInput;
+			}
+			dataStack.push(evalResult);		
 		}
 		return orgInput + " = " + dataStack.pop();
 	}
@@ -230,7 +375,19 @@ public class ExpressionEvaluator extends Application {
 		return str + "=" + dataStack.pop();
 
 	 */
-
+	
+	
+	private String addChar(String str, char itemToInsert, int position) {
+	    return str.substring(0, position) + itemToInsert + str.substring(position);
+	}
+	private boolean isNumber(String d) {
+		try {
+			Double.parseDouble(d);
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
+	}
 
 	private int getPrecedence(String op) {
 		if(op.equals("(")) {
@@ -243,7 +400,7 @@ public class ExpressionEvaluator extends Application {
 			return 1;
 		}
 	}
-	private int evaluateTopOfStack() {
+	private double evaluateTopOfStack() {
 		d2 = dataStack.pop();
 		d1 = dataStack.pop();
 		String op = operStack.pop();
@@ -287,7 +444,7 @@ private int evaluateStack() {
 	 */
 
 
-	private int calculate(String operator, int a, int b) {
+	private double calculate(String operator, double a, double b) {
 		if(operator.equals("+")) {
 			return a+b;
 		}
--- src/ExpressionEvaluatorTest.java
+++ src/ExpressionEvaluatorTest.java
@@ -1,11 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 class ExpressionEvaluatorTest {
 
 	@BeforeAll
@@ -25,52 +30,474 @@ class ExpressionEvaluatorTest {
 	}
 
 	@Test
-	void test() {
+	@Order(1)
+	void test_BasicFunction() {
 		ExpressionEvaluator exprEval = new ExpressionEvaluator();
 		String evalResults;
 		String[] results;
+		int testNum = 0;
+		
+		System.out.println("Basic Function Tests");
 		
 		// test #1
+		testNum++;
 		evalResults = exprEval.evaluateExpression("3+6*9-4");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 1\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("53"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("53.0"));
 		
 		// test #2 - with arbitrary spacing
+		testNum++;
 		evalResults = exprEval.evaluateExpression("3+ 6   * 9  -4");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 2\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("53"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("53.0"));
 		
 		// test #3
+		testNum++;
 		evalResults = exprEval.evaluateExpression("3*9*7-4");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 3\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("185"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("185.0"));
 		
 		// test #4
+		testNum++;
 		evalResults = exprEval.evaluateExpression("3*9*(7-4)");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 4\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("81"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("81.0"));
 		
 		// test #5
+		testNum++;
 		evalResults = exprEval.evaluateExpression("3*(9*7-4)");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 5\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("177"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("177.0"));
 		
 		// test #6
+		testNum++;
 		evalResults = exprEval.evaluateExpression("((6-7)*9+15/3)/4+((5*2)+7)");
 		results = evalResults.split("=");
 		results[1] = results[1].trim();
-		System.out.println("Test # 6\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
-		assertTrue(results[1].equals("16"));
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("16.0"));
+	
+		// test #7
+		testNum++;
+		evalResults = exprEval.evaluateExpression("1+30-5*6-1");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("0.0"));
+
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(9)");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("9.0"));
+
+	}
+	
+	@Test
+	@Order(2)
+	void test_DoubleTest() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;
+		String[] results;
+		int testNum =0;
+		Double drslt;
+		
+		System.out.println("Double Tests");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("1.0 + 30.0 -(5.0*6.0)-   1.0");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("0.0"));
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("1.0 + 30 -(5   *6.0)-   1.0");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertTrue(results[1].equals("0.0"));
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("15/4.5");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(3.33333,drslt,0.00005);  
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("15*3.14159/2");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(23.5619,drslt,0.00005);  
+	}
+
+	@Test
+	@Order(3)
+	void test_ImplicitMult() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;
+		String[] results;
+		int testNum =0;
+		Double drslt;
+		
+		System.out.println("Implicit Multiplication");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(9)(7)(3)");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(189,drslt,0.00005);  
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(9)7+3(3)");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(72,drslt,0.00005);  
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(9+7)3");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(48,drslt,0.00005);  
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9(2-7)");
+		results = evalResults.split("=");
+		drslt = Double.parseDouble(results[1].trim());
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(-45,drslt,0.00005);  
+	}
+
+	@Test
+	@Order(4)
+	void test_NegativeNumbers() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;
+		String[] results;
+		int testNum =0;
+		Double drslt;
+		
+		System.out.println("Negative Numbers");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("-5");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(-5.0, drslt, 0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("16.0+-5");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(11.0, drslt, 0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("16.0--5");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(21.0, drslt, 0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("16.0*-5");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(-80.0, drslt, 0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("16.0/-5");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(-3.2, drslt, 0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("16.0+(-5)");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+drslt+"\n\n");
+		assertEquals(11.000, drslt,0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("-5(90)");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(-450.0, drslt,0.00005);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("-5(-90)");
+		results = evalResults.split("=");
+		results[1] = results[1].trim();
+		drslt = Double.parseDouble(results[1]);
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+results[1]+"\n\n");
+		assertEquals(450.0, drslt,0.00005);
+
+	}
+
+	@Test
+	@Order(5)
+	void test_ParenErrorTest() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;;
+		int testNum =0;
+		boolean errMatch;
+		
+		System.out.println("Paren Error Tests");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(");
+		errMatch = evalResults.contains("Paren Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression(")");
+		errMatch = evalResults.contains("Paren Error");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("(6+ (9*15)");
+		errMatch = evalResults.contains("Paren Error");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+
+	
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("6+ (9*15))");
+		errMatch = evalResults.contains("Paren Error");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+
+	}
+
+	@Test
+	@Order(6)
+	void test_OpErrorTest() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;;
+		int testNum =0;
+		boolean errMatch;
+		
+		System.out.println("Op Error Tests");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("()");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("+ 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("* 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("/ 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("* 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 *");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 /");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 +");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 + + 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 + * 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 - - - 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("19 - - + 8");
+		errMatch = evalResults.contains("Op Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+	}
+	
+	@Test
+	@Order(7)
+	void test_DataErrorTest() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;;
+		int testNum =0;
+		boolean errMatch;
+		
+		System.out.println("Data Error Tests");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("# + 7");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9 + 7f");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9 * m445");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9(m445)");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9(4#45)");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9 445 + 5");
+		errMatch = evalResults.contains("Data Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+				
+	}
+	
+	@Test
+	@Order(8)
+	void test_Div0Test() {
+		ExpressionEvaluator exprEval = new ExpressionEvaluator();
+		String evalResults;;
+		int testNum =0;
+		boolean errMatch;
+		
+		System.out.println("Div0 Tests");
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9/0");
+		errMatch = evalResults.contains("Div0 Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+		
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9/(1+30-5*6-1)");
+		errMatch = evalResults.contains("Div0 Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
+
+		// test #
+		testNum++;
+		evalResults = exprEval.evaluateExpression("9/(1+30-5*6-1)+16/4");
+		errMatch = evalResults.contains("Div0 Error:");
+		System.out.println("Test # "+testNum+"\nExpression Results: "+evalResults+"\nResult: "+errMatch+"\n\n");
+		assertTrue(errMatch);
 		
 	}
 
